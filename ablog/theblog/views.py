9a914b4afdf9d82ed7fed512bf395b03a267d647
steffydulka <50639970+steffydulka@users.noreply.github.com>
2021-12-20 23:06:43 -0300

    Categorias

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, Category
 from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    #ordering = ['-post_date']
+    ordering = ['-id']
+
+def CategoryView(request, cats):
+    category_posts = Post.objects.filter(category=cats)
+    return render(request, 'categories.html', {'cats':cats, 'category_posts':category_posts})
+
 
 class ArticleDetailView(DetailView):
     model = Post
@@ -20,3 +28,8 @@ class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'update_post.html'
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
